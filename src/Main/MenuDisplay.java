6b45da7d2ee60700470158deac512d85f5e8ffac
Nicolas Pagola <nicolaspagola@outlook.com>
2025-11-12 20:08:31 -0300

    TP6

---
+++ src/Main/MenuDisplay.java
@@ -0,0 +1,20 @@
+package Main;
+
+
+public class MenuDisplay {
+  
+    
+    public void mostrarMenu() {
+        System.out.println("--- MENU DE GESTIÓN DE PRODUCTOS ---");
+        System.out.println("1. Crear Producto con Codigo de Barras");
+        System.out.println("2. Buscar Producto por ID");
+        System.out.println("3. Listar todos los Productos");
+        System.out.println("4. Actualizar Producto");
+        System.out.println("5. Eliminar Producto");
+        System.out.println("6. Buscar Producto por Codigo de Barras (Búsqueda especial)"); 
+        System.out.println("0. Salir");
+       
+    }
+}
+    
+// do {} while (opcion != 0);  opcional
